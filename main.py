739fb9b1f3736b4a3a0d953f7108db3bdccefe20
Rakesh Kundu <57815079+Rakesh6841@users.noreply.github.com>
2022-09-22 16:26:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr
+import webbrowser as wb
+
+r1 = sr.Recognizer()
+r2 = sr.Recognizer()
+r3 = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('[search edureka: search youtube]')
+    print('speak now')
+    audio = r1.listen(source)
+
+if 'googly' in r1.recognize_google(audio):
+    r1 = sr.Recognizer()
+    url = 'https://www.youtube.com/results?search_query='
+    with sr.Microphone() as source:
+        print('search your query')
+        audio = r2.listen(source)
+
+        try:
+            get = r1.recognize_google(audio)
+            print(get)
+            wb.get().open_new(url+get)
+        except sr.UnknownValueError:
+            print('error')
+        except sr.RequestError as e:
+            print('failed',format(e))
